04cbe8c1ab7deeed7328372450f06f3114c3ce31
Se7en <455732253@qq.com>
2022-06-22 16:19:52 +0800

    Update iisScaner.py

--- iisScaner.py
+++ iisScaner.py
@@ -1,41 +1,42 @@
 import requests
-import threading
-from queue import Queue
 from argparse import ArgumentParser
+from concurrent.futures import ThreadPoolExecutor
 
-def vlun(q,of):
-    f = open(of, 'a')
-    while q.empty() is not True:
-        url_target = q.get()    
-        try:
-            status_1 = requests.options(url_target + '/*~1****/a.aspx')  # an existed file/folder
-            status_2 = requests.options(url_target + '/l1j1e*~1****/a.aspx')  # not existed file/folder
-            if status_1.status_code == 404 and status_2.status_code == 200: 
-                print(str(url_target)+' is vulerable')
-                f.write(str(url_target) + '' + '\n')
-            else:
-                print(str(url_target)+' is no vulerable')
-        except Exception:
+requests.packages.urllib3.disable_warnings()
+
+def vlun(urltarget):
+    url_target = urltarget    
+    try:
+        status_1 = requests.options(url=url_target + '/*~1****/a.aspx', verify=False)  # an existed file/folder
+        status_2 = requests.options(url=url_target + '/l1j1e*~1****/a.aspx', verify=False)  # not existed file/folder
+        if status_1.status_code == 404 and status_2.status_code == 200: 
+            print(str(url_target)+' is vulerable')
+            with open(outputfile, 'a') as f:
+                try:
+                    f.write(str(url_target) + '' + '\n')
+                except:
+                    pass
+        else:
             pass
-        q.task_done()
+            #print(str(url_target)+' is no vulerable')
+    except Exception:
+        pass
+
 def Scanner(url_file,max_thread,output_file):
     url_list = []
-    global q
     if url_file is not None:
         url_list = list(set([i.replace("\n", "") for i in open(str(url_file), "r").readlines()]))
     else:
         exit()
-    with open(output_file, 'w'):
+    with open(outputfile, 'w'):
         pass
-    
-    q = Queue() 
+    l=[]
+    p = ThreadPoolExecutor(max_thread)
     for url in url_list:
-        q.put(url)
-    for index in range(max_thread):
-        thread = threading.Thread(target=vlun, args=(q,output_file))
-        # thread.daemon = True
-        thread.start()
-    q.join()
+        obj = p.submit(vlun, url,)
+        l.append(obj)
+    p.shutdown()
+
 
 if __name__ == '__main__':
     usageexample = '''
@@ -52,12 +53,13 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--thread', dest="max_threads", nargs='?', type=int, default=1, help="Max threads")
     parser.add_argument('-o', '--output-file', dest="output_file", help="Example: result.txt")
     args = parser.parse_args()
+    global outputfile
     try:
-        if args.url_file and args.max_threads and  args.output_file:
+        if args.url_file and args.max_threads and args.output_file:
             print(usageexample+'url_file:'+args.url_file+'| max_threads:'+str(args.max_threads)+'| output_file:'+args.output_file)
-            Scanner(args.url_file,args.max_threads,args.output_file)
+            outputfile = args.output_file
+            Scanner(args.url_file,args.max_threads,outputfile)
         else:
             print(usageexample)
     except Exception as e:
         print(e)
-
